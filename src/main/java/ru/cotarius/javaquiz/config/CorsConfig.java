f87b0777dbd93b895d1e3981f62fc2b5439cea18
Oleg Prokopenko <cotarius@yandex.ru>
2025-07-08 20:56:59 +0300

    изменения в CorsConfig

--- src/main/java/ru/cotarius/javaquiz/config/CorsConfig.java
+++ src/main/java/ru/cotarius/javaquiz/config/CorsConfig.java
@@ -9,7 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")  // Разрешить все домены (для теста)
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("https://javacoder.su", "http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
